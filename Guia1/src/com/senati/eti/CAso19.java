d2ad851f5b8a05d2d2984f672447f735d61133ee
ADMIN <ADMIN@192.168.1.5>
2021-03-24 14:25:15 -0500

    Desarrollo guia1 SENATI.

---
+++ Guia1/src/com/senati/eti/CAso19.java
@@ -0,0 +1,23 @@
+package com.senati.eti;
+import java.util.Scanner;
+public class CAso19 {
+
+	public static void main(String[] args) {
+		Scanner suma = new Scanner(System.in);
+
+		System.out.print("Ingrese lado 1........: ");
+		int a = suma.nextInt();
+
+		System.out.print("Ingrese lado 2........: ");
+		int b = suma.nextInt();	
+
+		System.out.print("Ingrese lado 3........: ");
+		int c = suma.nextInt();
+
+		int total = a + b + c;
+
+		System.out.println("El perimetro del triangulo es " +  total);
+
+	}
+
+}
---
+++ Guia1/src/com/senati/eti/Caso2_2.java
@@ -0,0 +1,20 @@
+package com.senati.eti;
+import java.util.Scanner;
+public class Caso2_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Ingrese número...:");
+		int n = sc.nextInt();
+
+		String mensaje = "Número es par";
+
+		if(n % 2 != 0)
+			mensaje = "Numero es impar";
+
+		System.out.println("\nMensaje....: " + mensaje);
+
+	}
+
+}
